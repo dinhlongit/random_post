8dedc2840cfffe360a8e927731619f49295604c5
dinhlongit <zingnewtop@gmail.com>
2020-06-27 12:25:02 +0800

    Updates

---
+++ precache-manifest.d14d765e3a4141159edb94c43e5d15a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe60fdb0ba95464d80cb82fe71b2f9f7",
+    "url": "/random_post/index.html"
+  },
+  {
+    "revision": "a623a711c50d31479a59",
+    "url": "/random_post/static/css/main.03866d47.chunk.css"
+  },
+  {
+    "revision": "6d7be806e9d565a4ccfe",
+    "url": "/random_post/static/js/2.4f5228f9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random_post/static/js/2.4f5228f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a623a711c50d31479a59",
+    "url": "/random_post/static/js/main.9031c7cd.chunk.js"
+  },
+  {
+    "revision": "7cdc067c29d682593dce",
+    "url": "/random_post/static/js/runtime-main.cacc5686.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random_post/static/media/logo.5d5d9eef.svg"
+  }
+]);
